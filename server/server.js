b1e981648e687d34131eddce12fc569821cf7679
Zovi <39067421+VaelinAlSorna@users.noreply.github.com>
2018-07-13 10:41:02 +0200

    Added GET/,DELETE/,PATCH, /article/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const {ObjectID} = require('mongodb');
 const express = require('express');
-const bodyParser = require('body-parser')
-const moment = require('moment')
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const _ = require('lodash');
 
 const {Article} = require('./models/article');
-const {mongoose} = require('./db/mongoose')
+const {mongoose} = require('./db/mongoose');
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(bodyParser.json());
 app.post('/article',  async (req, res) => {
    let currentDate =parseInt(moment().format('DMY'))
     try {
-        let article = new Article({
+        let newArticle = new Article({
             //_creatorId: new ObjectID(),
             // creator: 'Mike',
             createdAt: currentDate,
@@ -23,8 +24,8 @@ app.post('/article',  async (req, res) => {
             comments:[]
         });
 
-        const doc = await article.save();
-        res.send(doc);
+        const article = await newArticle.save();
+        res.send({article});
     } catch (e) {
         res.status(400).send(e);
     }
@@ -34,12 +35,70 @@ app.get('/articles', async (req, res) => {
     
     try {
         const allArticles = await Article.find();
-        res.send(allArticles);
+        res.send({allArticles});
     } catch (e) {
         res.status(400);
     }
 });
 
+app.get('/article/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    if (!ObjectID.isValid(id)){
+        return res.status(400).send();
+    }
+
+    try {
+        const article = await Article.findById(id);
+        if (!article) {
+            return res.status(404).send();
+        }
+        res.send({article});
+    } catch (e) {
+        res.status(400).send();
+    }
+});
+
+app.delete('/article/:id', async (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)){
+        return res.status(400).send();
+    }
+
+    try{
+        const removedArticle = await Article.findOneAndRemove({_id: id});
+        
+        if (!removedArticle) {
+            return res.status(404).send();
+        }
+
+        res.send({removedArticle});
+    } catch (e) {
+        res.status(400).send();
+    }
+});
+
+// It suppose that you pass both the title and text if not one of them will be null or if you pass empty body both of them will be undefined 
+app.patch('/article/:id', async(req, res) => {
+    const id = req.params.id;
+    const {title, text} = _.pick(req.body, ['title', 'text']);
+
+    if (!ObjectID.isValid(id)){
+        return res.status(400).send();
+    }
+
+    try {
+        const updatedArticle = await  Article.findOneAndUpdate({_id: id}, {title, text}, {new: true});
+        if (!updatedArticle) {
+            return res.status(404).send()
+        }
+        res.send({updatedArticle});
+    } catch (e) {
+        res.status(400);
+    }
+
+});
 
 app.listen(3000, () => {
     console.log(`Started on port 3000`);
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,5 +1,6 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server'); 
 const {Article} = require('./../models/article');
@@ -17,8 +18,8 @@ describe(' /POST article', () => {
         .send({title, text})
         .expect(200)
         .expect((res) => {
-            expect(res.body.title).toBe(title);
-            expect(res.body.text).toBe(text);
+            expect(res.body.article.title).toBe(title);
+            expect(res.body.article.text).toBe(text);
         })
         .end((err, res) => {
             if (err) {
@@ -54,14 +55,130 @@ describe(' /POST article', () => {
     });
 });
 
-describe('GET/ articles', () => {
+describe('GET/articles', () => {
     it('should fetch back all articles', (done) => {
         request(app)
         .get('/articles')
         .expect(200)
         .expect((res) => {
-            expect(res.body.length).toBe(2)
+            expect(res.body.allArticles.length).toBe(2)
         })
+        .end(done);
+    });
+});
+
+describe('GET/article/:id', () => {
+    it('should find article by its id', (done) => {
+        let article = articles[0];
+        let id = articles[0]._id;
+
+        request(app)
+        .get(`/article/${id}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.article.title).toBe(article.title);
+        })
+        .end(done);
+    });
+
+    it('should return 404 if id was not found in any article', (done) => {
+        let id = new ObjectID();
+
+        request(app)
+        .get(`/article/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 400 if non-valid id was provided', (done) => {
+        let id = 4597;
+
+        request(app)
+        .get(`/article/${id}`)
+        .expect(400)
+        .end(done);
+    });
+});
+
+describe('DELETE/article/:id', () => {
+    it('should delete article by id', (done) => {
+        let id = articles[0]._id;
+        let article = articles[0];
+
+        request(app) 
+        .delete(`/article/${id}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.removedArticle.title).toBe(article.title);
+        })
+        .end(async (err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            try {
+                const allArticles = await Article.find({});
+                expect(allArticles.length).toBe(1);
+                done();
+            } catch(e) {
+                done(e);
+            }
+        });
+    });
+
+    it('should return 404 if id is not matched', (done) => {
+        let id = new ObjectID();
+
+        request(app)
+        .delete(`/article/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 400 if non-valid id was provided', (done) => {
+        const id = 1236545;
+
+        request(app)
+        .delete(`/article/${id}`)
+        .expect(400)
+        .end(done);
+
+    });
+});
+
+describe('PATCH/article/:id', () => {
+    it('should update article', (done) => {
+        const id = articles[0]._id;
+        const article = articles[0];
+        const title = 'New title';
+        const text = 'New text';
+
+        request(app)
+        .patch(`/article/${id}`)
+        .send({title, text})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.updatedArticle.text).not.toBe(article.text);
+        })
+        .end(done);
+
+    });
+
+    it('should return 404 if id not found', (done) => {
+        const id = new ObjectID;
+
+        request(app)
+        .patch(`/article/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 400 if non-valid id was passed', (done) => {
+        const id = 1236545;
+
+        request(app)
+        .patch(`/article/${id}`)
+        .expect(400)
         .end(done);
     });
 });
