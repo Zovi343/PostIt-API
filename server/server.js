8d04cbb877a3f89fd5c4b085d78b9fe32d76ffeb
Zovi <39067421+VaelinAlSorna@users.noreply.github.com>
2018-09-05 16:22:15 +0200

    Trying to make it work on firefox

--- server/server.js
+++ server/server.js
@@ -19,15 +19,20 @@ const corsOptions = { // from here is solution https://github.com/axios/axios/is
     origin: ['http://localhost:8080', 'https://postit-right-now.herokuapp.com'],
     allowedHeaders: ['Accept-Version', 'Authorization', 'Credentials', 'Content-Type', 'x-auth'],
     exposedHeaders: ['x-auth'],
-}
+};
   
-app.all('*', cors(corsOptions))
+app.all('*', cors(corsOptions));
+
+app.all('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8000", "https://postit-right-now.herokuapp.com");
+    res.header("Access-Control-Allow-Headers");
+});
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('<h1> PostIt-API </h1><p>You can make varios requests to this API and that way store things into DB, but of course you need to have permission first, which you can get from Jakub Zovak ;) email: jakub.zovak343@gmail.com.</p>')
-})
+});
 
 app.post('/article', authenticate, async (req, res) => {
     try {
